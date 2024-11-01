4fd7017a549e20518e83b24eb298083ed349da6d
Paolo Paci <paolopci@yahoo.it>
2024-11-01 17:47:41 +0100

    initial project

---
+++ ContosoUniversity/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ContosoUniversity.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string LastName { get; set; }
+        public string FirstMidName { get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime EnrollmentDate { get; set; }
+        public virtual ICollection<Enrollment> Enrollments { get; set; }
+    }
+}
